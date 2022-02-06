4a596be60479b98aa625995babe801bd9df37544
kakhramonovv2662 <qahramonovismoil860@gmail.com>
2022-02-06 20:09:40 +0500

    code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let elWrapper = document.querySelector(".js-wrapper");
 
 function handleBtn() {
-  window.location = "http://127.0.0.1:5501/info.html";
+  window.location = "https://weather-about.netlify.app/info.html";
 }
 
 elWrapper.addEventListener("click", handleBtn);
